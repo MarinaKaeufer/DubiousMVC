1b924abbcc92753a4fba9ac84e3fe5b73a23bc97
Marina Kaeufer <marishutka@yahoo.com>
2022-06-20 19:55:45 -0400

    JAWSDB addon force re-build

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,6 @@
-// Import the Sequelize constructor from the library
-const Sequelize = require('sequelize');
-
 require('dotenv').config();
-
-// Create connection to our database, pass in your MySQL information for username and password
+const Sequelize = require('sequelize');
 let sequelize;
-
 if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
