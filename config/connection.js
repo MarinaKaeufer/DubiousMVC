515287cef67a83c9c248cab7dd7eb1e2f82ab052
Marina Kaeufer <marishutka@yahoo.com>
2022-06-20 19:46:45 -0400

    conection change

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,30 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: 'localhost',
-    dialect: 'mysql',
-    port: 3306,
-  }
-);
+var database = process.env.DB_NAME || 'dubious_mvc'
+
+var sequelize = ""
+
+if (process.env.DATABASE_URL) {
+    sequelize = new Sequelize(database)
+}
+else {
+    sequelize = new Sequelize(database, process.env.DB_USER || 'user',process.env.DB_PASSWORD || 'password', {
+        dialect: 'mysql'
+    });
+}
+
+
+
+// const sequelize = new Sequelize(
+//   process.env.DB_NAME || 'dubious_mvc',
+//   process.env.DB_USER,
+//   process.env.DB_PASSWORD,
+//   {
+//     host: 'localhost',
+//     dialect: 'mysql',
+//     port: 3306,
+//   }
+// );
 
 module.exports = sequelize;
